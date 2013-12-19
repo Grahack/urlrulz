13b40ea1f7278b3e15deab91069d900b6d838d8b
Christophe Gragnic <christophegragnic@gmail.com>
2013-12-19 23:25:11 +0100

    Removed the silly URL URL feature.
    Now in there is no content, a response only is expected. No comparison
    is done.

--- urlrulz.py
+++ urlrulz.py
@@ -22,16 +22,19 @@ def check(filename='urlz.txt'):
     for line in open(filename).read().split('\n'):
         splitted_line = line.split(' ', 1)
         if len(splitted_line) == 1:
-            url, expected_content = 2*splitted_line
+            url, expected_content = splitted_line[0], None
         else:
             url, expected_content = splitted_line
         if not url or url.startswith('#'):
             continue
-        expected_content = expected_content.lstrip()
-        actual_content = urllib2.urlopen(url).read().rstrip('\n')
-        if actual_content != expected_content:
-            tpl = """At {}, expected \n'{}' but got \n'{}'"""
-            exit(tpl.format(url, expected_content, actual_content))
+        if expected_content is not None:
+            expected_content = expected_content.lstrip()
+            actual_content = urllib2.urlopen(url).read().rstrip('\n')
+            if actual_content != expected_content:
+                tpl = """At {}, expected \n'{}' but got \n'{}'"""
+                exit(tpl.format(url, expected_content, actual_content))
+        else:
+            urllib2.urlopen(url)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
